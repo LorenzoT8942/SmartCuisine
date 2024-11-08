31d8be16ab2c4b1d63633acd5a8084953c690f78
giuliocapo <caporrogiulio@gmail.com>
2024-11-08 17:50:02 +0100

    ShoppingList first commit

---
+++ shoppingListManagement/src/main/java/com/shoppingList/shoppingListManagement/ShoppingListManagementApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingList.shoppingListManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingListManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingListManagementApplication.class, args);
+	}
+
+}
