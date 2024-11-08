f16f1abf11b4e17f726dc5f7429f789d4d12dbb7
Francesco Saverio <frasconocchia1@gmail.com>
2024-11-08 17:31:47 +0100

    create the docker-compose file, created the microservices of auth and profile and created db configuration

---
+++ authenticationManagement/src/test/java/com/authentication/authenticationManagement/AuthenticationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authentication.authenticationManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
