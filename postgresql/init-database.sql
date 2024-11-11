7980e5348ca3aeeec7d9b290e2c7aa2041f1bb83
fracchiooo <frasconocchia1@gmail.com>
2024-11-11 14:43:29 +0100

    adjusted the docker-compose

---
+++ postgresql/init-database.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE authentication_db;
+CREATE DATABASE userProfile_db;
+CREATE DATABASE shoppingList_db;
+CREATE DATABASE ingredientsStorage_db;
+CREATE DATABASE recipiesAndIngredient_db;
+CREATE DATABASE notification_db;
